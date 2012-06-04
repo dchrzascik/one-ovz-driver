77222c87e8a15edabb94b05e93bf68d9046008b3
Marta Rylko <rylko.marta@gmail.com>
2012-06-04 23:40:37 +0200

    Moved vzcpucheck probe to the separate file, and run it not like a cmd but externam program

--- src/var/remotes/im/ovz.d/ovz.rb
+++ src/var/remotes/im/ovz.d/ovz.rb
@@ -85,27 +85,6 @@ net_text.split(/\n/).drop(2).each{|line|
 $netrx = netrx.to_s()
 $nettx = nettx.to_s()
 
-# OpenVZ specific parameters:
-
-# cpu unit is a number calculated by OpenVZ with the help of the special algorithm 
-# Current CPU utilization (CURRENT_CPU_UTIL) show the total cpu current utilisation (in units) assigned to currently running containers and the host system processess 
-
-# (NODE_CPU_POWER)power of the node is a cpu power of all cpus available on the host mesured in the same units
-
-# if the CURRENT_CPU_UTIL < NODE_CPU_POWER it means that the node is under utilized and the containers can receive more cpu that they have guaranree to receive (if no cpu_limit for the containers is set) 
-# if CURRENT_CPU_UTIL > NODE_CPU_POWER, node is overcommited: the node promised more cpu units than the power of the Hardware Node. In this case, the containers might receive less power that they had been guaranted to receive
-
-
-ovz_cpucheck=`vzcpucheck`
-exit(-1) if $?.exitstatus != 0
-ovz_cpucheck = ovz_cpucheck.split(/\n/)
-
-$current_util = ovz_cpucheck[0].split(/:/)[1].strip
-$node_power = ovz_cpucheck[1].split(/:/)[1].strip
-
-print_info("HYPERVISOR","ovz")
-print_info("NODE_CPU_POWER",$node_power)
-print_info("CURRENT_CPU_UTIL",$current_util)
 
 print_info("TOTALMEMORY",$total_memory)
 print_info("USEDMEMORY",$used_memory)
